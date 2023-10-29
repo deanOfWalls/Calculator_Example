7786c1b60b62e9608f7d8b4bb6ec3e150d2d3f7e
deanOfWalls <deanofwalls@gmail.com>
2023-10-29 15:52:34 -0400

    Added more tests

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestExponentation.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestExponentation.java
@@ -13,7 +13,7 @@ public class TestExponentation {
         //When: Perform the calculation
         double operand1 = 9;
         double operand2 = 2;
-        double result = Math.pow(operand1, operand2);
+        double result = exponentiation.calculate(operand1, operand2);
 
         //Then: Check if the result is as expected
         double expected = 81;
